afc2c7ea5cda5acdae916a496b5daa4ae691bc0d
Jose <joseedusolsona@gmail.com>
2025-03-05 11:48:05 +0200

    Add PeopleController and update project structure
    - Introduced `PeopleController` with a basic `Index` action.
    - Updated `Practical2_u23708795.csproj` to include the new controller.
    - Added new folders: `HTML\Images` and `Views\People`.

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practical2_u23708795.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
